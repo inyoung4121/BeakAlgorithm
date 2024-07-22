11fd2e75defa402dda51fe33da2969681c805cbf
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-22 16:54:52 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 22336 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -16,28 +16,29 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr= new int[m];
+        arr = new int[m];
         visited = new boolean[n];
 
         dfs(0);
         System.out.println(sb);
     }
 
-    public static void dfs(int depth){
-        if(depth == m){
-            for(int num : arr){
-                sb.append(num+" ");
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
             }
             sb.append("\n");
             return;
         }
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i + 1;
-                dfs( depth + 1);
-                visited[i] = false;
-            }
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+            visited[i] = false;
+            
         }
     }
 }
