dd790fbe23adf848902f8ed3d51c91ae68cac0f0
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-24 13:18:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 112 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,39 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			list.add(i+1);
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
 
-		int n = 1;
-		int idx = (K*n-1) % list.size();
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-		while(N-- > 0){
-			
-			sb.append(list.remove(idx)).append(", ");
-			
-			if(list.isEmpty()) break;
-			
-			idx = (idx + K - 1) % list.size();
-			n++;
-		}
-		String str = sb.substring(0, sb.length()-2);
-		System.out.print(str);
-		System.out.print('>');
-		
-	}
+        for( int i = 1; i < n+1; i++ ) {
+            list.add(i);
+        }
+
+        int idx = 0;
+        sb.append("<");
+        for(int i=0; i<n; i++) {
+            idx = (idx + m - 1) % list.size();
+            sb.append(list.remove(idx)).append(", ");
+        }
+        String str = sb.substring(0, sb.length()-2);
+        System.out.print(str);
+        System.out.print('>');
+    }
 }
