26c96713b6e824391b6cfb0e9d94c232fcb11ec8
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-24 12:18:52 +0900

    [Silver III] Title: 프린터 큐, Time: 156 ms, Memory: 15288 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,48 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder output = new StringBuilder();
         int totalCase = Integer.parseInt(br.readLine());
 
-        while (totalCase-- >0){
+        while (totalCase-- > 0) {
             Queue<int[]> q = new LinkedList<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n= Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
 
-            int count =0 ;
+            int count = 0;
 
-            for(int i = 0; i < n; i++){
+            for (int i = 0; i < n; i++) {
                 q.offer(new int[]{i, Integer.parseInt(st.nextToken())});
             }
 
-            while(!q.isEmpty()){
+            while (!q.isEmpty()) {
                 int[] temp = q.poll();
                 boolean print = true;
-                for(int [] test : q){
-                    if(temp[1]<test[1]){
-                        print=false;
+                for (int[] test : q) {
+                    if (temp[1] < test[1]) {
+                        print = false;
                         break;
                     }
                 }
-                if(print){
+                if (print) {
                     count++;
-                    if(temp[0]==m){
-                        System.out.println(count);
+                    if (temp[0] == m) {
+                        output.append(count).append("\n");
                         break;
                     }
-                }else{
+                } else {
                     q.offer(temp);
                 }
             }
         }
+        System.out.print(output.toString());
     }
 }
