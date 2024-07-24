9cb66e6ca56e4932a17cf8e8f3e6e9ec43c03b73
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-24 17:05:53 +0900

    [Gold V] Title: 탑, Time: 848 ms, Memory: 87304 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -8,22 +8,24 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         Stack<int[]> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i < n + 1; i++) {
             int[] arr = new int[]{i, Integer.parseInt(st.nextToken())};
             while (!stack.isEmpty()) {
                 if (stack.peek()[1] < arr[1]) {
                     stack.pop();
                 } else {
-                    System.out.print(stack.peek()[0] + " ");
+                    sb.append(stack.peek()[0]).append(" ");
                     break;
                 }
             }
-            if(stack.isEmpty()){
-                System.out.print("0 ");
+            if (stack.isEmpty()) {
+                sb.append("0 ");
             }
             stack.push(arr);
         }
+
+        System.out.println(sb);
     }
-
 }
-
