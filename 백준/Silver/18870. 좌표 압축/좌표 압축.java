bc1c98d1375fbf6e3eb3fb76241365507f3a57ab
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-26 15:24:52 +0900

    [Silver II] Title: 좌표 압축, Time: 1776 ms, Memory: 238084 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,39 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-//최대 n^2 = 10^12
-//정렬 된 복사본을 만들고 몇번쨰인지 확인만?
-//맵을 만들고 작은놈들만?
+import java.io.IOException;
+ 
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Arrays;
+ 
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] copy = Arrays.stream(arr)
-                .distinct()
-                .sorted()
-                .toArray();
-
-        for (int i = 0; i < copy.length; i++) {
-            if (map.containsKey(copy[i])) {
-                continue;
-            }
-            map.put(copy[i], i);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(map.get(arr[i])).append(" ");
-        }
-        System.out.println(sb);
-    }
+ 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] origin = new int[N];	// 원본 배열
+		int[] sorted = new int[N];	// 정렬 할 배열
+		HashMap<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();	// rank를 매길 HashMap
+ 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			// 정렬할 배열과 원본 배열에 값을 넣어준다.
+			sorted[i] = origin[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 정렬 할 배열에 대해 정렬을 수행해준다.
+		Arrays.sort(sorted);
+		
+		
+		// 정렬 된 배열을 순회하면서 map에 넣어준다.
+		int rank = 0;
+		for(int v : sorted) {
+			/*
+			 *  이 때 만약 앞선 원소가 이미 순위가 매겨졌다면
+			 *  중복되면 안되므로, 원소가 중복되지 않을 때만
+			 *  map에 원소와 그에 대응되는 순위를 넣어준다.
+			 */
+			if(!rankingMap.containsKey(v)) {
+				rankingMap.put(v, rank);
+				rank++;		// map에 넣고나면 다음 순위를 가리킬 수 있도록 1을 더해준다.
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int key : origin) {
+			int ranking = rankingMap.get(key);	// 원본 배열 원소(key)에 대한 value(순위)를 갖고온다.
+			sb.append(ranking).append(' ');
+		}
+		
+		System.out.println(sb);
+		
+		
+ 
+	}
 }
-
