de55d3e58ac3c7a2e5ea2276caa2ad5d2d25b309
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-29 16:40:51 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1100 ms, Memory: 257808 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,24 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static final int INF = -1;
-
     public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
-
-    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] edges = new List[n+1];
-        for (int i = 1; i <= n; i++) edges[i] = new ArrayList<>();
+        List<Integer>[] edges = new List[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -26,32 +23,33 @@ public class Main {
             edges[a].add(b);
         }
 
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, INF);
-        Queue<Integer> q = new ArrayDeque<>();
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        Queue<Integer> q = new LinkedList<>();
         q.add(x);
         dist[x] = 0;
 
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
         while (!q.isEmpty()) {
             int cur = q.poll();
             if (dist[cur] > k) break;
-            if (dist[cur] == k) answer.add(cur);
+            if (dist[cur] == k) result.add(cur);
 
             for (int next : edges[cur]) {
-                if (dist[next] != INF) continue;
-                dist[next] = dist[cur]+1;
+                if (dist[next] != -1) continue;
+                dist[next] = dist[cur] + 1;
                 q.add(next);
             }
         }
 
-        Collections.sort(answer);
+        Collections.sort(result);
         StringBuilder sb = new StringBuilder();
-        for (int cur : answer) {
+        for (int cur : result) {
             sb.append(cur).append('\n');
         }
 
-        System.out.print(answer.isEmpty() ? -1 : sb);
+        System.out.print(result.isEmpty() ? -1 : sb);
     }
 }
+
