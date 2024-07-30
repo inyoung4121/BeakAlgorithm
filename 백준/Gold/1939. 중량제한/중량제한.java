9144f705473d40bab4f0a77ca66bcf77ed7602af
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-30 12:56:29 +0900

    [Gold III] Title: 중량제한, Time: 560 ms, Memory: 49612 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,24 +1,52 @@
 import java.io.*;
 import java.util.*;
- 
-public class Main {
-    static int atoi(String str) {
-        return Integer.parseInt(str);
+
+class Node implements Comparable<Node> {
+    int from, to, wei;
+
+    public Node(int from, int to, int wei) {
+        this.from = from;
+        this.to = to;
+        this.wei = wei;
     }
-    static int N, M;
-    static ArrayList<Node> A = new ArrayList<>();
-    static int start, end, max = Integer.MIN_VALUE;
+
+
+    @Override
+    public int compareTo(Node o) {
+        return -this.wei + o.wei;
+    }
+}
+
+public class Main {
+
     static int [] parent;
     public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
- 
-    static void pro() {
-        Collections.sort(A, (o1, o2)->{
-            return o2.wei - o1.wei;
-        });
- 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N+1];
+        for(int i = 1; i <= N; i++) parent[i] = i;
+
+        ArrayList<Node> A = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int wei = Integer.parseInt(st.nextToken());
+
+            A.add(new Node(from, to, wei));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        Collections.sort(A);
+
         for (Node n : A) {
             union(n.from, n.to);
             if(find(start) == find(end)){
@@ -27,60 +55,19 @@ public class Main {
             }
         }
     }
- 
     static void union(int u, int v) {
         u = find(u);
         v = find(v);
- 
         if(u == v) return;
- 
-        if(u > v){
-            parent[u] = v;
-        }
-        else{
-            parent[v] = u;
-        }
+        if(u > v) parent[u] = v;
+        else parent[v] = u;
+
     }
- 
+
     static int find(int v) {
         if(v == parent[v]) return v;
- 
+
         parent[v] = find(parent[v]);
         return parent[v];
-    }
- 
- 
-    static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        N = atoi(st.nextToken());
-        M = atoi(st.nextToken());
- 
-        parent = new int[N+1];
-        for(int i = 1; i <= N; i++) parent[i] = i;
- 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = atoi(st.nextToken());
-            int to = atoi(st.nextToken());
-            int wei = atoi(st.nextToken());
- 
-            A.add(new Node(from, to, wei));
-        }
- 
-        st = new StringTokenizer(br.readLine());
-        start = atoi(st.nextToken());
-        end = atoi(st.nextToken());
-    }
- 
-    static class Node {
-        int from, to, wei;
- 
-        public Node(int from, int to, int wei) {
-            this.from = from;
-            this.to = to;
-            this.wei = wei;
-        }
     }
 }
