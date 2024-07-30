a926d07f62ac7b0229383fdb1920ba422304f3e0
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-30 22:57:50 +0900

    [Gold I] Title: 3에 깃든 힘, Time: 920 ms, Memory: 113320 KB -BaekjoonHub

---
+++ 백준/Gold/25622. 3에 깃든 힘/3에 깃든 힘.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] prv, to, las, sub;
+    static int sz, cnt;
+    static int[][] ans;
+    static int[] ptr;
+
+    static void initialize(int n) {
+        int maxNodes = n + 1;
+        int maxEdges = 2 * n;
+        prv = new int[maxEdges];
+        to = new int[maxEdges];
+        las = new int[maxNodes];
+        sub = new int[maxNodes];
+        ans = new int[n / 3 + 1][3];
+        ptr = new int[n / 3 + 1];
+    }
+
+    static void addEdge(int u, int v) {
+        sz++;
+        to[sz] = v;
+        prv[sz] = las[u];
+        las[u] = sz;
+    }
+
+    static void dfs(int x, int p) {
+        sub[x] = 1;
+        for (int i = las[x]; i != 0; i = prv[i]) {
+            if (to[i] != p) {
+                dfs(to[i], x);
+                sub[x] += sub[to[i]];
+            }
+        }
+        if (sub[x] % 3 == 0) cnt++;
+    }
+
+    static void trace(int x, int p, int grp) {
+        if (sub[x] % 3 == 0) grp = ++cnt;
+        ans[grp][ptr[grp]++] = x;
+        for (int i = las[x]; i != 0; i = prv[i]) {
+            if (to[i] != p) {
+                trace(to[i], x, grp);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+
+        if (n % 3 != 0) {
+            bw.write("U\n");
+            bw.flush();
+            return;
+        }
+
+        initialize(n);
+
+        for (int i = 1; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            addEdge(u, v);
+            addEdge(v, u);
+        }
+
+        dfs(1, -1);
+
+        if (cnt != n / 3) {
+            bw.write("U\n");
+            bw.flush();
+            return;
+        }
+
+        cnt = 0;
+        trace(1, -1, 0);
+
+        bw.write("S\n");
+        for (int i = 1; i <= n / 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                bw.write(ans[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
