4315c341bd0ca5309ad9b7ef7b9114dbee7db039
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-31 13:57:07 +0900

    [Silver IV] Title: 30, Time: 184 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -6,16 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] arr = br.readLine().toCharArray();
         Arrays.sort(arr);
+        if(arr[0] != '0'){
+            System.out.println(-1);
+            return;
+        }
         int sum = 0;
         StringBuilder sb = new StringBuilder();
         for (int i = arr.length - 1; i >= 0; i--) {
             sum += arr[i] - '0';
             sb.append(arr[i]);
-            if( i==0 && arr[i] != '0'){
-                System.out.println(-1);
-                return;
-            }
         }
         System.out.println(sum % 3 == 0 ? sb : -1);
     }
 }
+
+//16544	192
