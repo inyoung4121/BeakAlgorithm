814fe76416851d589cdeb4a90e2cf0ae4f0dbcd2
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-31 16:06:42 +0900

    [Gold V] Title: 회문, Time: 340 ms, Memory: 28804 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -1,46 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        while (n-- > 0) {
+            String s = br.readLine();
+            int result = checkPalindrome(s);
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-        StringBuilder answer = new StringBuilder();
-        while (T-- > 0) {
-            String input = br.readLine();
-            StringBuilder sb = new StringBuilder(input);
-            String result = "2";
-
-            // 회문
-            if (input.equals(sb.reverse().toString())) {
-                result = "0";
-            }
-
-            // 유사회문
-            else {
-                int left = 0;
-                int right = input.length() - 1;
-                while (left < right) {
-                    if (input.charAt(left) != input.charAt(right)) {
-                        // 왼쪽, 오른쪽 각각 지워봄
-                        StringBuilder delLeft = new StringBuilder(input).deleteCharAt(left);
-                        StringBuilder delRight = new StringBuilder(input).deleteCharAt(right);
-
-                        // 둘중 하나라도 성사되면 유사회문
-                        if (delLeft.toString().equals(delLeft.reverse().toString()) || delRight.toString().equals(delRight.reverse().toString())) {
-                            result = "1";
-                        }
-                        break;
-                    }
-                    left++;
-                    right--;
+    private static int checkPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                if (isPalindrome(s, left + 1, right)) {
+                    return 1;
+                } else if (isPalindrome(s, left, right - 1)) {
+                    return 1;
+                } else {
+                    return 2;
                 }
             }
-            answer.append(result).append("\n");
         }
-        answer.deleteCharAt(answer.length() - 1);
-        System.out.print(answer);
-   }
+        return 0;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
