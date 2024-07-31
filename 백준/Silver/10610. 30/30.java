e8a00febf334ab9efc236b49f0d5436da2a39caa
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-31 13:55:58 +0900

    [Silver IV] Title: 30, Time: 192 ms, Memory: 16544 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,38 +1,21 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static final int MAX = 100000;
-    
-    public static String str;
-    public static int[] numCountArr;
-    public static long strLen;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        str= sc.nextLine();
-        
-        strLen = str.length();
-        numCountArr = new int[10];
-        long total = 0;
-        for(int i=0; i < strLen; i++) {
-            int tNum = Integer.parseInt(str.substring(i, i+1));
-            numCountArr[tNum] += 1;
-            total+=tNum;
-        }
-        
-        // 0이 존재하지 않으면 30 배수 조차도 될 수 없다.
-        // 각 자리수의 총 합이 3의 배수가 아니면 종료해야한다.
-        if(!str.contains("0") || total % 3 != 0) {
-            System.out.println("-1");
-            return;
-        }
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+        int sum = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i = 9; i >= 0; i--) {
-            while(numCountArr[i] > 0) {
-                sb.append(i);
-                numCountArr[i]--;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sum += arr[i] - '0';
+            sb.append(arr[i]);
+            if( i==0 && arr[i] != '0'){
+                System.out.println(-1);
+                return;
             }
         }
-        System.out.println(sb.toString());
+        System.out.println(sum % 3 == 0 ? sb : -1);
     }
 }
