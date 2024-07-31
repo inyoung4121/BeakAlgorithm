b327f1f5da0d7d82abbf4258e48e9bbc4fbe0463
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-07-31 17:48:21 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
