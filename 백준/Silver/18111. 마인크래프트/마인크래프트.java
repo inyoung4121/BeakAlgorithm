cc3ccad93759417448232438aa84793d0f2e8bd1
inyoung4121 <167910692+inyoung4121@users.noreply.github.com>
2024-08-01 15:41:13 +0900

    [Silver II] Title: 마인크래프트, Time: 412 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,58 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-class Main{
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		
-		int[][] minecraft = new int[N][M];
-		int min = Integer.MAX_VALUE;
-		int max = -1;
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0; j<M; j++) {
-				minecraft[i][j] = Integer.parseInt(st.nextToken());
-				min = Math.min(min, minecraft[i][j]);
-				max = Math.max(max, minecraft[i][j]);
-			}
-		}
-		
-		int min_sec = Integer.MAX_VALUE;
-		int high_block = 0;
-		for(int i=min; i<=max; i++) {
-			int second = 0;
-			int block = B;
-			
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<M; k++) {
-					if(i > minecraft[j][k]) {
-						second += i-minecraft[j][k];
-						block -= i-minecraft[j][k];
-					}
-					else if(i < minecraft[j][k]) {
-						second += (minecraft[j][k]-i)*2;
-						block += minecraft[j][k]-i;
-					}
-				}
-			}
-			if(block < 0) continue;
-			
-			if(min_sec >= second) {
-				min_sec = second;
-				high_block = Math.max(high_block, i);
-			}
-		}
-		
-		System.out.println(min_sec + " " + high_block);		
-	}
+        // 높이별 개수
+        int[] heightCounts = new int[257];
+        // 최소 높이
+        int min = Integer.MAX_VALUE;
+        // 최대 높이
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int height = Integer.parseInt(st.nextToken());
+                heightCounts[height]++;
+                min = Math.min(min, height);
+                max = Math.max(max, height);
+            }
+        }
+
+        int minTime = Integer.MAX_VALUE;
+        int bestHeight = -1;
+
+        for (int targetHeight = min; targetHeight <= max; targetHeight++) {
+            int time = 0;
+            int inventory = b;
+
+            for (int height = 0; height <= 256; height++) {
+                if (heightCounts[height] > 0) {
+                    if (height > targetHeight) {
+                        time += (height - targetHeight) * heightCounts[height] * 2;
+                        inventory += (height - targetHeight) * heightCounts[height];
+                    } else if (height < targetHeight) {
+                        time += (targetHeight - height) * heightCounts[height];
+                        inventory -= (targetHeight - height) * heightCounts[height];
+                    }
+                }
+            }
+
+            if (inventory >= 0 && time <= minTime) {
+                minTime = time;
+                bestHeight = targetHeight;
+            }
+        }
+
+        System.out.println(minTime + " " + bestHeight);
+    }
 }
